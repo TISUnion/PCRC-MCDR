a7412305b6786fcc4703d0df8a115dd3e681240c
Fallen_Breath <yqx791125833@gmail.com>
2021-01-15 02:47:49 +0800

    should work for MCDR 1.0 i guess not tested yet xd

--- PCRC-MCDR.py
+++ PCRC-MCDR.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
-import sys
 import importlib
 import time
 
-sys.path.append('plugins/')
+from mcdreforged.api.all import *
+
+PLUGIN_METADATA = {
+    'id': 'pcrc_mcdr',
+    'version': '1.0.0',
+    'name': 'PCRC MCDR',
+    'description': 'a MCDR plugin wrapper for PCRC',
+    'author': 'Fallen_Breath',
+    'link': 'https://github.com/TISUnion/PCRC-MCDR'
+}
+
 PCRC = None
 PREFIX = '!!PCRC'
 
 # 0=guest 1=user 2=helper 3=admin
 Permission = 1
 
-def permission(server, info, perm):
+
+def permission_check(server, info, perm):
     if info.is_user:
         if info.source == 1:
             return True
@@ -18,23 +28,26 @@ def permission(server, info, perm):
             return True
     return False
 
+
 def load_PCRC():
     global PCRC
     PCRC = importlib.import_module('PCRC-MCDR.PCRC')
 
 
-def on_info(server, info):
-    if permission(server, info, Permission) and info.content == '!!PCRC start':
-        server.reply(info, 'Starting PCRC')
-        if PCRC.is_working():
-            server.reply(info, 'PCRC is already running!')
-        else:
-            PCRC.start()
-    if info.source == 1 and info.content == '!!PCRC stop':
-        if PCRC.is_working():
-            PCRC.stop()
-        else:
-            server.reply(info, 'PCRC is not running!')
+@new_thread('PCRC-MCDR')
+def on_user_info(server, info):
+    if PCRC is not None:
+        if permission_check(server, info, Permission) and info.content == '!!PCRC start':
+            server.reply(info, 'Starting PCRC')
+            if PCRC.is_working():
+                server.reply(info, 'PCRC is already running!')
+            else:
+                PCRC.start()
+        if info.source == 1 and info.content == '!!PCRC stop':
+            if PCRC.is_working():
+                PCRC.stop()
+            else:
+                server.reply(info, 'PCRC is not running!')
 
 
 def on_load(server, old):
@@ -48,7 +61,7 @@ def on_load(server, old):
         load_PCRC()
 
 
-def on_mcdr_stop(server):
+def stopping(server):
     global PCRC
     if PCRC is None:
         return
@@ -63,3 +76,12 @@ def on_mcdr_stop(server):
                         time.sleep(0.1)
         if not PCRC.is_stopped():
             server.logger.info('PCRC took too long to stop (more than 10min)! Exit anyway')
+
+
+def on_mcdr_stop(server):
+    stopping(server)
+
+
+@new_thread('PCRC-MCDR')
+def on_remove(server):
+    stopping(server)
